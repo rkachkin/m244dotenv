66b5bf0a55053122406bf2bbf2ce79d32c2f1463
Rodion Kachkin <r.kachkin@togas.com>
2022-07-26 12:26:50 +0400

    Configure environment settings

---
+++ app/etc/env.php
@@ -0,0 +1,184 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => $_ENV['BACKEND_NAME']
+    ],
+    'remote_storage' => [
+        'driver' => 'file'
+    ],
+    'queue' => [
+        'amqp' => [
+            'host' => 'rabbitmq',
+            'port' => '5672',
+            'user' => 'guest',
+            'password' => 'guest',
+            'virtualhost' => '/'
+        ],
+        'consumers_wait_for_messages' => 1
+    ],
+    'crypt' => [
+        'key' => $_ENV['CRYPT_KEY'], // 14c92108e9fecaffac743ac87cf1f7bc
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'db',
+                'dbname' => 'magento',
+                'username' => 'magento',
+                'password' => 'magento',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'driver_options' => [
+                    1014 => false
+                ],
+                'profiler' => [
+                    'class' => 'Magento\\Framework\\DB\\Profiler',
+                    'enabled' => $_ENV['DB_DEBUG']
+                ]
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => $_ENV['MAGE_MODE'],
+    'http_cache_hosts' => [
+        [
+            'host' => 'varnish',
+            'port' => '80'
+        ]
+    ],
+    'session' => [
+        'save' => 'redis',
+        'redis' => [
+            'host' => 'redis',
+            'port' => '6379',
+            'password' => '',
+            'timeout' => '2.5',
+            'persistent_identifier' => '',
+            'database' => '2',
+            'compression_threshold' => '2048',
+            'compression_library' => 'gzip',
+            'log_level' => '1',
+            'max_concurrency' => '20',
+            'break_after_frontend' => '5',
+            'break_after_adminhtml' => '30',
+            'first_lifetime' => '600',
+            'bot_first_lifetime' => '60',
+            'bot_lifetime' => '7200',
+            'disable_locking' => '0',
+            'min_lifetime' => '60',
+            'max_lifetime' => '2592000',
+            'sentinel_master' => '',
+            'sentinel_servers' => '',
+            'sentinel_connect_retries' => '5',
+            'sentinel_verify_master' => '0'
+        ]
+    ],
+    'cache' => [
+        'frontend' => [
+            'default' => [
+                'id_prefix' => '69d_',
+                'backend' => 'Magento\\Framework\\Cache\\Backend\\Redis',
+                'backend_options' => [
+                    'server' => 'redis',
+                    'database' => '0',
+                    'port' => '6379',
+                    'password' => '',
+                    'compress_data' => '1',
+                    'compression_lib' => ''
+                ]
+            ],
+            'page_cache' => [
+                'id_prefix' => '69d_',
+                'backend' => 'Magento\\Framework\\Cache\\Backend\\Redis',
+                'backend_options' => [
+                    'server' => 'redis',
+                    'database' => '1',
+                    'port' => '6379',
+                    'password' => '',
+                    'compress_data' => '0',
+                    'compression_lib' => ''
+                ]
+            ]
+        ],
+        'allow_parallel_generation' => false
+    ],
+    'lock' => [
+        'provider' => 'db',
+        'config' => [
+            'prefix' => null
+        ]
+    ],
+    'directories' => [
+        'document_root_is_pub' => true
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => $_ENV['APP_ENV'] === 'production',
+        'block_html' => $_ENV['APP_ENV'] === 'production',
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => $_ENV['APP_ENV'] === 'production',
+        'config_webservice' => 1,
+        'translate' => 1
+    ],
+    'downloadable_domains' => [
+
+    ],
+    'install' => [
+        'date' => 'Tue, 26 Jul 2022 07:51:15 +0000'
+    ],
+    'db_logger' => [
+        'output' => $_ENV['DB_LOGGER'] ? 'enabled' : 'disabled',
+        'log_everything' => 1,
+        'query_time_threshold' => '0.001',
+        'include_stacktrace' => 1
+    ],
+    'system' => [
+        'default' => [
+            'web' => [
+                'unsecure' => [
+                    'base_url' => 'https://app.m244dotenv.test/'
+                ],
+                'secure' => [
+                    'base_url' => 'https://app.m244dotenv.test/',
+                    'offloader_header' => 'X-Forwarded-Proto',
+                    'use_in_frontend' => '1',
+                    'use_in_adminhtml' => '1'
+                ],
+                'seo' => [
+                    'use_rewrites' => '1'
+                ]
+            ],
+            'system' => [
+                'full_page_cache' => [
+                    'caching_application' => '2',
+                    'ttl' => '604800'
+                ]
+            ],
+            'catalog' => [
+                'search' => [
+                    'enable_eav_indexer' => '1'
+                ]
+            ],
+            'dev' => [
+                'static' => [
+                    'sign' => '0'
+                ]
+            ]
+        ]
+    ]
+];
